9d650114d9e89c195ed872d6f444280dd78d2d41
artofmayhem <chef_tony_miller@yahoo.com>
2021-10-08 12:28:31 -1000

    added header video

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.scss";
 import { Nav, Home, AboutUs, Technology } from "./components";
+import {HeaderVideo} from './assets/index'
+import ReactPlayer from 'react-player'
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Nav />
+        <ReactPlayer
+            url={HeaderVideo}
+            playing={true}
+            loop={true}
+            height={'100%'}
+            width={'100%'}
+            style={{margin: 'auto auto', }}
+            id={'video-player'}
+            playsinline={true}
+        />
       </div>
       <Switch>
         <Route exact path="/" component={Home} />
