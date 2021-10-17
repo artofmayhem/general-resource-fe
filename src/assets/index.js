91313cff35870329349b8883007e56be2daabde0
artofmayhem <chef_tony_miller@yahoo.com>
2021-10-16 23:43:26 -1000

    added headline and second informational section

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,7 +1,9 @@
 import HeaderVideo from './headerVideo.mp4';
 import Sunrise from './sunrise.jpg';
+import IslandRoad from './islandRoad.jpg';
 
 export {
     HeaderVideo,
-    Sunrise
+    Sunrise,
+    IslandRoad
 }
